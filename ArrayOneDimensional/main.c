3f305e24b847ff7112f980b53588ade1d4fbbdd7
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-03-13 18:27:34 +0300

    Add files via upload

--- ArrayOneDimensional/main.c
+++ ArrayOneDimensional/main.c
@@ -1,9 +1,12 @@
 /*
-    Задание 1.3. Нестандартное применение одномерных массивов
-    Король, №2, поле №8
+*   Задание 1.3. Нестандартное применение одномерных массивов
+*   Король, №2, поле №8
+*   Калабин Данил
+*   ИВТ-13БО
 */
 #include <stdio.h>
 #include <stdlib.h>
+
 
 // Ищет в какой строке указанный элемент
 int searchStrEl(int *A,int strs, int elem) {
@@ -13,22 +16,41 @@ int searchStrEl(int *A,int strs, int elem) {
         indR = i*i-1;
         indL = (i-1)*(i-1);
         
-        if(elem >= indL && elem <= indR) {
+        if(elem >= indL && elem <= indR)
             str = i;
-        }
     }
 
     return str;
 }
 
-void step(int *A, int n, int a, int b) {
-    int strB, currStr;
+// Проверка элемента на крайность
+int idxCheck(int currStr, int el) {
+    int ind;
 
-    printf("%d ", a);
-    strB = searchStrEl(A, n, b);
+    ind = (currStr-1)*(currStr-1);
+    
+    // Проверка на левый крайний
+    if(ind == el)
+        return 1;
+    
+    // Проверка на правый крайний
+    ind += (currStr-1)*2;
+    if(ind == el)
+        return 2;
+
+    // Ни левый крайний и не правый
+    return 0;    
+}
+
+// Функция хода фигуры
+void step(int *A, int n, int a, int b) {
+    int strFinish, currStr;
+
+    printf("%d ", A[a]);
+    strFinish = searchStrEl(A, n, b);
 
     currStr = searchStrEl(A, n, a);
-    if(currStr == strB) {
+    if(currStr == strFinish) {
         if(a == b)
             return;
 
@@ -38,148 +60,56 @@ void step(int *A, int n, int a, int b) {
         if(a > b)
             step(A,n,--a,b);
     }
-    if(currStr < strB) {
+    if(currStr < strFinish) {
         a += currStr*2;
         currStr++;
         step(A,n,a,b);
     }
-    if(currStr > strB) {
+    if(currStr > strFinish) {
+        if(idxCheck(currStr, a) == 1) {
+            a++;
+            printf("%d ", A[a]);
+        }
+        
+        if(idxCheck(currStr, a) == 2) {
+            a--;
+            printf("%d ", A[a]);
+        }        
+
         currStr--;
         a -= currStr*2;
         step(A,n,a,b);
     }
 }
 
-
 void main() {
-    int i,a, b, n, x, countEl, *A, *B, *C;
+    int i, start, finish, str, x, countEl, *A;
 
     printf("Введите кол-во строк поля\n");
-    if(!(scanf("%d", &n)))
+    if(!(scanf("%d", &str)) || !str)
         return;
+    
+    // Всего элементов на поле
+    countEl = str*str;
 
     printf("Введите начальное положение\n");
-    if(!(scanf("%d", &a)))
+    if(!(scanf("%d", &start)) || start <= 0 || start > countEl)
         return;
 
-    printf("Введите конченое положение\n");
-    if(!(scanf("%d", &b)))
+    printf("Введите конечное положение\n");
+    if(!(scanf("%d", &finish)) || finish <= 0 || finish > countEl)
         return;
-    b -= 1;
-    a -= 1;
-
-    countEl = n*n;
+    start -= 1;
+    finish -= 1;
 
     if(!(A = (int*)malloc(countEl * sizeof(int))))
         return;
-
-    
-
-    printf("Всего элементов: %d\n", countEl);
     x = 0;
     for (i = 0; i < countEl; i++) 
         A[i] = ++x;
 
-    step(A,n,a,b);
+    step(A,str,start,finish);
 
-/*
-    // Нашли строку в которой указанный начальный элемент и крайние элементы в этой строке
-    for(i = 0; i <= n; i++) {
-        indRa = i*i-1;
-        indLa = (i-1)*(i-1);
-        
-        if(a >= indLa && a <= indRa) {
-            strA = i;
-            printf("Начальное в %d строке\n", strA);
-        }
-    }
-
-    // Нашли строку в которой указанный конечный элемент и крайние элементы в этой строке
-    for(i = 0; i <= n; i++) {
-        indRb = i*i-1;
-        indLb = (i-1)*(i-1);
-        
-        if(b >= indLb && b <= indRb) {
-            strB = i;
-            printf("Конечная в %d строке\n", strB);
-        }
-    }
-
-    if (a == b) {
-        printf("Вы дебилы!");
-        return;
-    }
-
-    if(strA == strB) {
-        if(a < b)
-            for(i = a; i <= b; i++) 
-                printf("%d ", i);        
-        if(a > b)
-           for(i = a; i >= b; i--) 
-                printf("%d ", i);  
-        
-    }
-    
-    if(strA < strB) {
-        curr = a;
-        for(i = strA; i < strB; i++) {
-            printf("%d ", curr);
-            curr += i*2;
-        }
-        
-        if(curr < b)
-            for(i = curr; i <= b; i++) 
-                printf("%d ", i);    
-
-        if(curr > b)
-           for(i = curr; i >= b; i--) 
-                printf("%d ", i); 
-
-        if (curr == b)
-            printf("%d ", curr);
-
-    }
-
-    if(strA > strB) {
-        curr = a;
-        for(i = strA; i < strB; i--) {
-            printf("%d ", curr);
-            curr -= (i-1)*2;
-        }
-        
-        if(curr < b)
-            for(i = curr; i <= b; i++) 
-                printf("%d ", i);    
-
-        if(curr > b)
-           for(i = curr; i >= b; i--) 
-                printf("%d ", i); 
-
-        if (curr == b)
-            printf("%d ", curr);
-    }
-*/
-/*
-    for(i = n; i > 0; i--) {
-        indR = (i-1)*(i-1)-1;
-        indL = indR + 1;
-        printf("%d ", indL);
-        indD = indL;
-        for(j = i; j < n; j++) {
-            indD += j*2;
-            printf("%d ", indD);
-        }
-    }
-    for(i = 2; i <= n; i++) {
-        indR = i*i-1;
-        indD = indR;
-        printf("%d ", indR);
-        for(j = i; j < n; j++) {
-            indD += j*2;
-            printf("%d ", indD); 
-        }
-    }
-*/
-
+    free(A);
     return;
 }
