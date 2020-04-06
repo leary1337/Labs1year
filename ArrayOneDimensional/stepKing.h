f07905055e29ce72326f23189efd850b0dc34b45
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-04-06 10:50:21 +0300

    Update stepKing.h

--- ArrayOneDimensional/stepKing.h
+++ ArrayOneDimensional/stepKing.h
@@ -1,6 +1,9 @@
 #ifndef STEPKING_H
 #define STEPKING_H
+// Ищет в какой строке указанный элемент
 void step(int *, int , int , int );
+// Проверка элемента на крайность
 int idxCheck(int , int );
+// Функция хода фигуры
 int searchStrEl(int *,int , int );
 #endif // STEPKING_H
