eed666fb585fb370dc2682895dda39ab4b337d38
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-04-21 00:06:36 +0300

    Complete

--- FindArticulationPoint/articulation_point_search.c
+++ FindArticulationPoint/articulation_point_search.c
@@ -1,50 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include "articulation_point_search.h"
 
+// Prepare and complete the task
 void prepare_for_task(int **adjencency_Matrix, int countVertices) {
 	int i;
-	// "timer" for determining the times
-	int visitTime = 0;
-	// tin[i] - time to go deep into vertex i, 
-	// low[i] - minimum from the time of entry to the very peak tin[i]
-	int *time_in; 
+	visitTime = 0;
 
-	if(!(time_in = (int*)malloc(countVertices * sizeof(int))))
-		return;
+	/*	Explanation of Variables:
+	* disc, low - tags,
+	* visited(bool) - mark visited peaks, ArtPoints(bool) - mark articulation points
+	*/
+
+	int *disc, *low, *parent;
+	int *visited, *ArtPoints;
+
+	disc = (int*)malloc(countVertices * sizeof(int));
+	low = (int*)malloc(countVertices * sizeof(int));
+	parent = (int*)malloc(countVertices * sizeof(int));
+	visited = (int*)malloc(countVertices * sizeof(int));
+	ArtPoints = (int*)malloc(countVertices * sizeof(int));
+
+	// Variable initialization
+	for (i = 0; i < countVertices; i++) {
+		disc[i] = -1;
+		low[i] = infinite;
+		parent[i] = -1;
+		visited[i] = 0;
+		ArtPoints[i] = 0;
+	}
 
 	// Let's go do the task
-	do_depth_first_search(0, -1, adjencency_Matrix, countVertices, time_in, visitTime);
+	dfs_ap(adjencency_Matrix, disc, low, visited, parent, ArtPoints, 0, countVertices);
+	print_art_point(ArtPoints, countVertices);
 }
 
-// Depth-first search in graph
-int do_depth_first_search(int vertex, int parent, int **adjencency_Matrix, int countVertices, int *time_in, int visitTime) {
-	int i, min_time, x, y, tmp;
+// Depth search and articulation point calculation
+void dfs_ap(int **adjMat, int *disc, int *low, int *visited, int *parent, int *ArtPoints, int vertex, int countVertices) {
+	int i;
 
-	time_in[vertex] = ++visitTime;
-	min_time = time_in[vertex];
-	x = 0;
-	y = (parent != -1);
-	printf("STEP");
+	visited[vertex] = 1;
+	visitTime = disc[vertex] = low[vertex] = visitTime + 1;
+	int child = 0;
+	for (i = 0; i < countVertices; i++) {
+		if (adjMat[vertex][i] == 1) {
+			if (visited[i] == 0) {
 
-	for(i = 0; i < countVertices; i++) {
-		if(adjencency_Matrix[vertex][i]) {
-			if(!time_in[i]) { // Not visited yet
-				tmp = do_depth_first_search(i, vertex, adjencency_Matrix, countVertices, time_in, visitTime);
-				if(tmp >= time_in[vertex])
-					++x;
+				child = child + 1;
+				parent[i] = vertex;
+				dfs_ap(adjMat, disc, low, visited, parent, ArtPoints, i, countVertices);
+				low[vertex] = MIN(low[vertex], low[i]);
+
+				// Check if the root is an Articulation point or not
+				if (parent[vertex] == -1 && child > 1)
+					ArtPoints[vertex] = 1;
+				// Check if the other nodes are Articulation points or not
+				if (parent[vertex] != -1 && low[i] >= disc[vertex] && low[i] != infinite)
+					ArtPoints[vertex] = 1;
 			}
-			else 
-				tmp = time_in[i];
-			min_time = min(min_time, tmp);	
+			else if (parent[vertex] != i)
+				low[vertex] = MIN(low[vertex], disc[i]);
 		}
 	}
-	if(x + y >= 2)
-		printf("IS_CUTPOINT");
-
-	return min_time;
 }
 
-void is_cutpoint(int vertex) {
-	printf("%c", 'A'+vertex);
+// Display the result of the program
+void print_art_point(int *ArtPoints, int countVertices) {
+	int i;
+	for(i = 0; i < countVertices; i++)
+		if(ArtPoints[i])
+			printf("\nArticulation Point: %c", 'A'+i);
 }
