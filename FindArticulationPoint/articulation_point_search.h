7c2b63ec12985a5d0908426f303acf64bdb947d2
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-04-21 00:06:50 +0300

    Update articulation_point_search.h

--- FindArticulationPoint/articulation_point_search.h
+++ FindArticulationPoint/articulation_point_search.h
@@ -1,11 +1,26 @@
 #ifndef ARTICULATION_POINT_SEARCH
 #define ARTICULATION_POINT_SEARCH
-#define min(x, y) ( (x) < (y) ? (x) : (y) )
+#define MIN(x, y) ( (x) < (y) ? (x) : (y) )
 
+// Make this bigger as possible if you work for graphs that have more than 1000000000 node
+// TODO change according to the maximum number of nodes
+#define infinite 1000000000
+
+// Entry time
+int visitTime;
+
+// Prepare and complete the task
 void prepare_for_task(int **adjencency_Matrix, int countVertices);
 
-int do_depth_first_search(int vertex, int parent, int **adjencency_Matrix, int countVertices, int *timer_in, int visitTime);
+// Depth search and articulation point calculation
+	/*	Explanation of Variables:
+	* adj - Adjencency Matrix,
+	* disc, low - tags,
+	* visited(bool) - mark visited peaks, ArtPoints(bool) - mark articulation points
+	*/
+void dfs_ap(int **adj, int *disc, int *low, int *visited, int *parent, int *ArtPoints, int vertex, int countVertices);
 
-void is_cutpoint(int vertex);
+// Display the result of the program
+void print_art_point(int *ArtPoints, int countVertices);
 
 #endif // ARTICULATION_POINT_SEARCH
