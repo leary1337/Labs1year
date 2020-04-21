842ae29c5abd296cdafde66a3fb70d6dba9e628a
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-04-21 10:26:07 +0300

    Update articulation_point_search.c

--- FindArticulationPoint/articulation_point_search.c
+++ FindArticulationPoint/articulation_point_search.c
@@ -4,47 +4,40 @@
 #include "articulation_point_search.h"
 
 // Prepare and complete the task
-void prepare_for_task(int **adjencency_Matrix, int countVertices) {
+void prepare_and_run(int **adjencencyMatrix, int countVertices) {
 	int i;
 	visitTime = 0;
 
 	/*	Explanation of Variables:
 	* disc, low - tags,
-	* visited(bool) - mark visited peaks, ArtPoints(bool) - mark articulation points
+	* visited(bool) - mark visited peaks, artPoints(bool) - mark articulation points
 	*/
 
 	int *disc, *low, *parent;
-	int *visited, *ArtPoints;
+	int *visited, *artPoints;
 
 	disc = (int*)malloc(countVertices * sizeof(int));
 	low = (int*)malloc(countVertices * sizeof(int));
 	parent = (int*)malloc(countVertices * sizeof(int));
 	visited = (int*)malloc(countVertices * sizeof(int));
-	ArtPoints = (int*)malloc(countVertices * sizeof(int));
+	artPoints = (int*)malloc(countVertices * sizeof(int));
 
 	// Variable initialization
 	for (i = 0; i < countVertices; i++) {
 		disc[i] = -1;
-		low[i] = infinite;
+		low[i] = INF;
 		parent[i] = -1;
 		visited[i] = 0;
-		ArtPoints[i] = 0;
+		artPoints[i] = 0;
 	}
 
 	// Let's go do the task
-	dfs_ap(adjencency_Matrix, disc, low, visited, parent, ArtPoints, 0, countVertices);
-	print_art_point(ArtPoints, countVertices);
-
-	// Clear memory
-	free(disc);
-	free(low);
-	free(parent);
-	free(visited);
-	free(ArtPoints);
+	dfs_ap(adjencencyMatrix, disc, low, visited, parent, artPoints, 0, countVertices);
+	print_ap(artPoints, countVertices);
 }
 
 // Depth search and articulation point calculation
-void dfs_ap(int **adjMat, int *disc, int *low, int *visited, int *parent, int *ArtPoints, int vertex, int countVertices) {
+void dfs_ap(int **adjMat, int *disc, int *low, int *visited, int *parent, int *artPoints, int vertex, int countVertices) {
 	int i;
 
 	visited[vertex] = 1;
@@ -56,15 +49,15 @@ void dfs_ap(int **adjMat, int *disc, int *low, int *visited, int *parent, int *A
 
 				child = child + 1;
 				parent[i] = vertex;
-				dfs_ap(adjMat, disc, low, visited, parent, ArtPoints, i, countVertices);
+				dfs_ap(adjMat, disc, low, visited, parent, artPoints, i, countVertices);
 				low[vertex] = MIN(low[vertex], low[i]);
 
 				// Check if the root is an Articulation point or not
 				if (parent[vertex] == -1 && child > 1)
-					ArtPoints[vertex] = 1;
+					artPoints[vertex] = 1;
 				// Check if the other nodes are Articulation points or not
-				if (parent[vertex] != -1 && low[i] >= disc[vertex] && low[i] != infinite)
-					ArtPoints[vertex] = 1;
+				if (parent[vertex] != -1 && low[i] >= disc[vertex] && low[i] != INF)
+					artPoints[vertex] = 1;
 			}
 			else if (parent[vertex] != i)
 				low[vertex] = MIN(low[vertex], disc[i]);
@@ -73,9 +66,18 @@ void dfs_ap(int **adjMat, int *disc, int *low, int *visited, int *parent, int *A
 }
 
 // Display the result of the program
-void print_art_point(int *ArtPoints, int countVertices) {
+void print_ap(int *artPoints, int countVertices) {
 	int i;
 	for(i = 0; i < countVertices; i++)
-		if(ArtPoints[i])
+		if(artPoints[i])
 			printf("\nArticulation Point: %c", 'A'+i);
+}
+
+// Clear memory
+void clear_all(int *disc, int* low, int *visited, int *parent, int *artPoints) {
+	free(disc);
+	free(low);
+	free(parent);
+	free(visited);
+	free(artPoints);
 }
