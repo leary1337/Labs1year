020e119dcbed1ed5083085eec8fb32d2bda686b8
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-04-21 10:25:41 +0300

    Complete all

--- FindArticulationPoint/input_output.c
+++ FindArticulationPoint/input_output.c
@@ -3,25 +3,25 @@
 #include <stdlib.h>
 
 // Initialization Adjencency Matrix (allocation memory and init with zero)
-int **init_adjencency_Matrix(int **adjencency_Matrix, int countVertices) {
+int **init_adjencencyMatrix(int **adjencencyMatrix, int countVertices) {
 	int i, j;
 
 	// Allocation memory for Adjencency Matrix and initialization Matrix with zero
-	if(!(adjencency_Matrix = (int**)malloc(countVertices * sizeof(int*))))
+	if(!(adjencencyMatrix = (int**)malloc(countVertices * sizeof(int*))))
 		return NULL;
 	for(i = 0; i < countVertices; i++) { 
-		if(!(adjencency_Matrix[i] = (int*)malloc(countVertices * sizeof(int))))
+		if(!(adjencencyMatrix[i] = (int*)malloc(countVertices * sizeof(int))))
 			return NULL;
 
 		for(j = 0; j < countVertices; j++)
-			adjencency_Matrix[i][j] = 0;
+			adjencencyMatrix[i][j] = 0;
 	}
 
-	return adjencency_Matrix;
+	return adjencencyMatrix;
 }
 
 // Filling Adjencency Matrix (user)
-int **fill_adjencency_Matrix(int **adjencency_Matrix, int countVertices) {
+int **fill_adjencencyMatrix(int **adjencencyMatrix, int countVertices) {
 	int i, j, tmp;
 
 	printf("Fill in the adjacency matrix:\n");
@@ -34,15 +34,15 @@ int **fill_adjencency_Matrix(int **adjencency_Matrix, int countVertices) {
 
 			if(!scanf("%d", &tmp) && tmp < 0) 
 				return NULL;
-			adjencency_Matrix[i][j] = adjencency_Matrix[j][i] = tmp;
+			adjencencyMatrix[i][j] = adjencencyMatrix[j][i] = tmp;
 		}
 	}
 
-	return adjencency_Matrix;
+	return adjencencyMatrix;
 }
 
 // Output Adjencency Matrix to the console
-void display_adjencency_Matrix(int **adjencency_Matrix, int countVertices) {
+void display_adjencencyMatrix(int **adjencencyMatrix, int countVertices) {
 	int i, j;
 
 	printf("\n\nAdjacency Matrix:\n  ");
@@ -52,14 +52,14 @@ void display_adjencency_Matrix(int **adjencency_Matrix, int countVertices) {
 	for (i = 0; i < countVertices; i++) {
 		printf("\n%c ", 'A' + i);
 		for (j = 0; j < countVertices; j++) {
-			printf("%d ", adjencency_Matrix[i][j]);
+			printf("%d ", adjencencyMatrix[i][j]);
 		}
 	}
 	printf("\n");
 }
 
 // Getting the number of vertices in the graph from the User
-int get_num_vertices() {
+int get_numVertices() {
 	int countVertices;
 
 	printf("Input number of Vertices: ");
