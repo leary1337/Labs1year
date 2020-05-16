6cd225b044d7c44501fd2c717be0bfc703cbfadb
leary1337 <59512774+leary1337@users.noreply.github.com>
2020-05-16 12:30:48 +0300

    Упс, забыл добавить файлы, в которых лежит сам алгоритм описанный в прошлом коммите - mixed.*

---
+++ ChemicalExp/mixed.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+#include "mixed.h"
+
+using namespace std;
+
+// Функция смешивания элементов по таблице взаимодействия
+// Параметры:
+// chainElem - элементы которые будем класть в пробирку,
+// numMixed - кол-во таких элементов,
+// table - таблица взаимодействия элементов,
+// testTube - пробирка (непосредственно стэк),
+// numPairs - кол-во пар элементов в таблице, 
+// 	которые образуют взаимодействие (кол-во строк в таблице)
+void mixed(int *chainElem, int numMixed, int **table, stack <int> &testTube, int numPairs) {
+	int i, j;
+	bool check;
+
+	for (i = 1; i < numMixed; i++) {
+		check = true;
+		for (j = 0; j < numPairs; j++) {
+			if (testTube.top() == table[j][0] && chainElem[i] == table[j][1]) {
+				testTube.pop();
+				testTube.push(table[j][2]);
+				check = true;
+				break;
+			}
+			else 
+				check = false;
+		}
+		if(!check)
+			testTube.push(chainElem[i]);
+	}
+
+	return;
+}
+
+// Функция изначально кладет первый элемент в пробирку и далее запускает функцию mixed, 
+// 	которая продолжит класть элементы и проверять их на взаимодействие, если оно обнаружено в таблице
+// 		тогда смешаем и запишем результат
+stack <int> put_elem(int *chainElem, int numMixed, int **table, int numPairs) {
+	stack <int>  testTube;
+
+	testTube.push(chainElem[0]);
+
+	mixed(chainElem, numMixed, table, testTube, numPairs);
+
+	return testTube;
+}
+
